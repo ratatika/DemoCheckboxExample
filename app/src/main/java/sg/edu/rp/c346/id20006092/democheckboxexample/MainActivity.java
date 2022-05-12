9a7b349bc4facb719bfad4a6c2a3706d77297d9d
RITIKA GURUNG <20006092@myrp.edu.sg>
2022-05-12 19:41:41 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20006092/democheckboxexample/MainActivity.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id20006092.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cbEnabled = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_LONG).show();
+
+                if (cbEnabled.isChecked() == true) {
+
+                    cbEnabled.setEnabled(true);
+                    String message = "The discount is given.";
+                    tvShow.setText(message);
+
+                } else {
+
+                    cbEnabled.setEnabled(false);
+                    String message = "The discount is not given.";
+                    tvShow.setText(message);
+
+                }
+
+            }
+        });
+
+
+    }
+}
